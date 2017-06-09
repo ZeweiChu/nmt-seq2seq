fd55135e0957af6dd128764c0d5338ec7b6647e8
Zewei Chu <zeweichu@gmail.com>
2017-06-09 08:33:28 -0500

    add demo

--- run.sh
+++ run.sh
@@ -2,4 +2,4 @@ model_dir="seq2seq"
 if [ ! -d $model_dir ]; then
 	mkdir $model_dir
 fi
-python main.py --train_file data/train.txt --dev_file data/dev.txt --test_file data/test.txt --batch_size 128 --num_epoches 20 --model_file $model_dir/model.th --model EncoderDecoderModel --learning_rate 0.01 --embedding_size 200 --hidden_size 200 --eval_epoch 1 --vocab_file $model_dir/vocab.pkl --use_cuda 1
+python main.py --train_file data/train.txt --dev_file data/dev.txt --test_file data/test.txt --batch_size 64 --num_epoches 20 --model_file $model_dir/model.th --model EncoderDecoderModel --learning_rate 0.01 --embedding_size 200 --hidden_size 200 --eval_epoch 1 --vocab_file $model_dir/vocab.pkl --use_cuda 1
