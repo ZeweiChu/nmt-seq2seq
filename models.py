8c4cdeac3732dcc51234ad7f4357083fbe2689c4
ZEWEI CHU <zeweichu@gmail.com>
2017-06-29 20:12:02 -0700

    add code

--- models.py
+++ models.py
@@ -12,14 +12,13 @@ class EncoderDecoderModel(nn.Module):
     def __init__(self, args):
         super(EncoderDecoderModel, self).__init__()
         self.nhid = args.hidden_size
-        self.nlayers = args.num_layers
 
         self.embed_en = nn.Embedding(args.en_total_words, args.embedding_size)
         self.embed_cn = nn.Embedding(args.cn_total_words, args.embedding_size)
         self.encoder = nn.LSTMCell(args.embedding_size, args.hidden_size)
         self.decoder = nn.LSTM(args.embedding_size, args.hidden_size, batch_first=True)
 
-        self.linear = nn.Linear(self.nhid, args.vocab_size)
+        self.linear = nn.Linear(self.nhid, args.cn_total_words)
         self.linear.bias.data.fill_(0)
         self.linear.weight.data.uniform_(-0.1, 0.1)
 
@@ -28,10 +27,11 @@ class EncoderDecoderModel(nn.Module):
 
     def init_hidden(self, bsz):
         weight = next(self.parameters()).data
-        return (Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()),
-                Variable(weight.new(self.nlayers, bsz, self.nhid).zero_()))
+        return (Variable(weight.new(bsz, self.nhid).zero_()),
+                Variable(weight.new(bsz, self.nhid).zero_()))
 
     def forward(self, x, x_mask, y, hidden):
+        
         x_embedded = self.embed_en(x)
         B, T, embedding_size = x_embedded.size()
         # encoder
@@ -49,6 +49,9 @@ class EncoderDecoderModel(nn.Module):
         c = cells.gather(1, x_lengths).permute(1,0,2)
 
         # decoder
+        
+        y_embedded = self.embed_cn(y)
+        # code.interact(local=locals())
         hiddens, (h, c) = self.decoder(y_embedded, hx=(h, c))
 
         hiddens = hiddens.contiguous()
@@ -56,4 +59,3 @@ class EncoderDecoderModel(nn.Module):
         decoded = self.linear(hiddens.view(hiddens.size(0)*hiddens.size(1), hiddens.size(2)))
         decoded = F.log_softmax(decoded)
         return decoded.view(hiddens.size(0), hiddens.size(1), decoded.size(1)), hiddens
-
